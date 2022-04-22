628099b7314ac366371900b26ff26e0a5446a12c
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2022-04-22 03:12:49 +0200

    ..

--- CNN_Code/CNN.py
+++ CNN_Code/CNN.py
@@ -2,10 +2,15 @@
 """
 Created on Thu Apr 21 22:34:38 2022
 
-@author: HOME
+@author: tasneem
 """
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+from keras import layers
+from keras.preprocessing.sequence import pad_sequences
 
 filepath_dict = {'yelp':   'yelp_labelled.txt',
                  'amazon': 'amazon_cells_labelled.txt',
@@ -19,3 +24,52 @@ for source, filepath in filepath_dict.items():
 
 df = pd.concat(df_list)
 print(df.iloc[0])
+
+ 
+
+df_yelp = df[df['source'] == 'yelp']
+
+sentences = df_yelp['sentence'].values
+y = df_yelp['label'].values
+
+sentences_train, sentences_test, y_train, y_test = train_test_split(
+    sentences, y, test_size=0.25, random_state=1000)
+
+
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(sentences_train)
+
+X_train = tokenizer.texts_to_sequences(sentences_train)
+X_test = tokenizer.texts_to_sequences(sentences_test)
+
+vocab_size = len(tokenizer.word_index) + 1  # Adding 1 because of reserved 0 index
+
+embedding_dim = 100
+maxlen = 100
+
+X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
+X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+
+
+model = Sequential()
+model.add(layers.Embedding(input_dim=vocab_size, 
+                           output_dim=embedding_dim, 
+                           input_length=maxlen))
+model.add(layers.Conv1D(128, 5, activation='relu'))
+model.add(layers.GlobalMaxPool1D())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+history = model.fit(X_train, y_train,
+                    epochs=50,
+                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
